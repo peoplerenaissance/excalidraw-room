2cafbb0bc91e49574b114e34c729471f0b8d0ad4
cheng <ummcheng@users.noreply.github.com>
2022-06-25 15:14:05 +0000

    upgrade node to 18, update dependencies, refactor index.ts, clean up unused bits

---
+++ Makefile
@@ -0,0 +1,2 @@
+dev:
+	docker compose run --service-ports base "bash"
--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,42 @@
-import debug from "debug";
 import express from "express";
 import http from "http";
-import socketIO from "socket.io";
-
-const serverDebug = debug("server");
-const ioDebug = debug("io");
-const socketDebug = debug("socket");
-
-require("dotenv").config(
-  process.env.NODE_ENV !== "development"
-    ? { path: ".env.production" }
-    : { path: ".env.development" },
-);
+import { Server } from "socket.io";
 
 const app = express();
-const port = process.env.PORT || 80; // default port to listen
+const port = process.env.PORT;
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.send("Excalidraw collaboration server is up :)");
+app.get("/", (_: any, res: any) => {
+  res.send("200");
 });
 
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  serverDebug(`listening on port: ${port}`);
+  console.log(`listening on port: ${port}`);
 });
 
-const io = socketIO(server, {
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin":
-        (req.header && req.header.origin) || "https://excalidraw.com",
-      "Access-Control-Allow-Credentials": true,
-    };
-    res.writeHead(200, headers);
-    res.end();
-  },
-});
+const io = new Server(server, {});
 
 io.on("connection", (socket) => {
-  ioDebug("connection established!");
   io.to(`${socket.id}`).emit("init-room");
+
   socket.on("join-room", (roomID) => {
-    socketDebug(`${socket.id} has joined ${roomID}`);
+    console.log(`${roomID}: ${socket.id} join-room`);
     socket.join(roomID);
-    if (io.sockets.adapter.rooms[roomID].length <= 1) {
+    const room = io.sockets.adapter.rooms.get(roomID)!;
+    if (room.size <= 1) {
+      console.log(`${roomID}: ${socket.id} first-in-room`);
       io.to(`${socket.id}`).emit("first-in-room");
     } else {
+      console.log(`${roomID}: ${socket.id} new-user`);
       socket.broadcast.to(roomID).emit("new-user", socket.id);
     }
-    io.in(roomID).emit(
-      "room-user-change",
-      Object.keys(io.sockets.adapter.rooms[roomID].sockets),
-    );
+    io.in(roomID).emit("room-user-change", Array.from(room));
   });
 
   socket.on(
     "server-broadcast",
     (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-      socketDebug(`${socket.id} sends update to ${roomID}`);
       socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
     },
   );
@@ -69,7 +44,6 @@ io.on("connection", (socket) => {
   socket.on(
     "server-volatile-broadcast",
     (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-      socketDebug(`${socket.id} sends volatile update to ${roomID}`);
       socket.volatile.broadcast
         .to(roomID)
         .emit("client-broadcast", encryptedData, iv);
@@ -78,14 +52,17 @@ io.on("connection", (socket) => {
 
   socket.on("disconnecting", () => {
     const rooms = io.sockets.adapter.rooms;
-    for (const roomID in socket.rooms) {
-      const clients = Object.keys(rooms[roomID].sockets).filter(
+    Array.from(socket.rooms).forEach((roomID) => {
+      console.log(`${roomID}: ${socket.id} disconnecting`);
+
+      const clients = Array.from(rooms.get(roomID) || []).filter(
         (id) => id !== socket.id,
       );
-      if (clients.length > 0) {
+      if (clients) {
+        console.log(`${roomID}: room-user-change ${clients}`);
         socket.broadcast.to(roomID).emit("room-user-change", clients);
       }
-    }
+    });
   });
 
   socket.on("disconnect", () => {
